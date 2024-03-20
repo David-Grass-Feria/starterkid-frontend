21b79c213154eb108343b79124b25b03dcc2fd41
David <kontakt@david-grass-feria.de>
2024-03-20 19:21:20 +0100

    Update homepage.blade.php

--- resources/views/livewire/front/homepage.blade.php
+++ resources/views/livewire/front/homepage.blade.php
@@ -21,21 +21,7 @@
 
 
 
-<x-starterkid-frontend::features.feature-with-image-left 
-heading="{{config('starterkid-frontend.hero_h1')}}" 
-description="{{config('starterkid-frontend.hero_description')}}" 
-imgSrc="https://tierschutzverein-muenchen.de/img/containers/assets/v1/startseite/shop-startseite-finish.jpg/8afa9ab3541f90875830f0d7814585b0.jpg" 
-imgAlt="{{config('starterkid-frontend.hero_image_alt')}}"
-buttonPrimaryRoute="{{config('starterkid-frontend.hero_button_primary_route')}}" 
-buttonPrimaryAnchor="{{config('starterkid-frontend.hero_button_primary_anchor')}}" /> 
 
-<x-starterkid-frontend::features.feature-with-image-right 
-heading="{{config('starterkid-frontend.hero_h1')}}" 
-description="{{config('starterkid-frontend.hero_description')}}" 
-imgSrc="https://tierschutzverein-muenchen.de/img/containers/assets/v1/startseite/shop-startseite-finish.jpg/8afa9ab3541f90875830f0d7814585b0.jpg" 
-imgAlt="{{config('starterkid-frontend.hero_image_alt')}}"
-buttonPrimaryRoute="{{config('starterkid-frontend.hero_button_primary_route')}}" 
-buttonPrimaryAnchor="{{config('starterkid-frontend.hero_button_primary_anchor')}}" /> 
 
 
 
