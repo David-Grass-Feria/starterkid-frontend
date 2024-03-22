9b02ac92aa061ad5bfc17e3c741111227268e239
David <kontakt@david-grass-feria.de>
2024-03-22 16:05:51 +0100

    1.0

--- resources/views/components/image-credits.blade.php
+++ resources/views/components/image-credits.blade.php
@@ -1,3 +1,5 @@
 @if(isset($imageCredits))
-<a href="{{$imageCredits}}" target="_blank" rel="nofollow" {{$attributes->merge(['class' => 'text-xs text-gray-600 bg-white p-0.5 absolute left-4 bottom-1'])}}>{{$imageCredits}}</a>
+@if($imageCredits !== '')
+<a href="{{$imageCredits}}" target="_blank" rel="nofollow" {{$attributes->merge(['class' => 'text-xs text-gray-600 bg-white p-0.5 absolute left-4 bottom-1'])}}>© {{$imageCredits}}</a>
+@endif
 @endif
