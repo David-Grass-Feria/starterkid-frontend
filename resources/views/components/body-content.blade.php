52b868636af18cea83ca8fe4ee1122b5ad28bbc9
David <kontakt@david-grass-feria.de>
2024-03-28 21:15:11 +0100

    Update body-content.blade.php

--- resources/views/components/body-content.blade.php
+++ resources/views/components/body-content.blade.php
@@ -133,7 +133,7 @@ $content = preg_replace('/<img(.*?)height=".*?"(.*?)width=".*?"(.*?)src="(.*?)"(
      <div class="mt-5 relative">
         <img width="600" height="400" class="w-full max-w-[400px] xl:max-w-[600px]" 
         srcset="
-        {{$imgSrcMedium}} 360w,
+        {{$imgSrcMedium}} 300w,
         {{$imgSrc}} 600w" 
         src="{{$imgSrc}}" alt="{{$imgAlt}}" />
         <x-starterkid-frontend::image-credits imageCredits="{{$imageCredits}}"/>
