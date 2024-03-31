37fad4e4107b01a0bac6e8efad86c63a006584d3
David <kontakt@david-grass-feria.de>
2024-03-31 19:20:51 +0200

    Update card-grid-service-item.blade.php

--- resources/views/components/card-grid-service-item.blade.php
+++ resources/views/components/card-grid-service-item.blade.php
@@ -1,3 +1,4 @@
+<article>
 <div class="relative flex items-center space-x-3 rounded-lg border border-primary bg-white px-6 py-5">
     <div class="flex-shrink-0">
       <img width="40" height="40" class="h-10 w-10 rounded-3xl object-contain" src="{{$imgSrc}}" alt="{{$imgAlt}}">
@@ -10,4 +11,4 @@
       </a>
     </div>
   </div>
-
+</article>
