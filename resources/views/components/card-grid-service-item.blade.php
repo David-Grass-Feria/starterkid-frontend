35b3082fbb2a2a36dd66d14ff4522e44c0663174
David <kontakt@david-grass-feria.de>
2024-03-31 14:47:10 +0200

    Create card-grid-service-item.blade.php

---
+++ resources/views/components/card-grid-service-item.blade.php
@@ -0,0 +1,12 @@
+<div class="relative flex items-center space-x-3 rounded-lg border border-gray-300 bg-white px-6 py-5">
+    <div class="flex-shrink-0">
+      <img width="40" height="40" class="h-10 w-10 rounded-3xl object-contain" src="{{$imgSrc}}" alt="{{$imgAlt}}">
+    </div>
+    <div class="min-w-0 flex-1 text-font_primary">
+      <a href="{{$href}}" title="{{$hrefTitle}}" class="focus:outline-none">
+        <span class="absolute inset-0" aria-hidden="true"></span>
+        <p class="text-lg font-bold">{{$heading}}</p>
+        <p class="truncate text-xs">{!! $preview !!}</p>
+      </a>
+    </div>
+  </div>
