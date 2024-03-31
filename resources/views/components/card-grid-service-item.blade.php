5ead9b672a161f8ea4ca616a727e54ea566827ab
David <kontakt@david-grass-feria.de>
2024-03-31 19:57:04 +0200

    Update card-grid-service-item.blade.php

--- resources/views/components/card-grid-service-item.blade.php
+++ resources/views/components/card-grid-service-item.blade.php
@@ -1,4 +1,4 @@
-<div wire:key="{{$cardKey}}" class="relative flex items-center space-x-3 rounded-lg border border-primary bg-white px-6 py-5">
+<div class="relative flex items-center space-x-3 rounded-lg border border-primary bg-white px-6 py-5">
 <div>
  
     <div class="flex-shrink-0">
