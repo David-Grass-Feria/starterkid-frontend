4933de6fee533bea3bdf3e7d26206b103a60d1b0
David <kontakt@david-grass-feria.de>
2024-03-31 14:53:06 +0200

    1.0

--- resources/views/components/navlink-mobile.blade.php
+++ resources/views/components/navlink-mobile.blade.php
@@ -2,7 +2,7 @@
 
 @php
 $classes = ($active ?? false)
-            ? '-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-font_primary underline decoration-bg-primary p-3'
+            ? '-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-font_primary underline decoration-text-primary p-3'
             : '-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-font_primary';
 @endphp
 
--- resources/views/components/navlink.blade.php
+++ resources/views/components/navlink.blade.php
@@ -2,7 +2,7 @@
 
 @php
 $classes = ($active ?? false)
-            ? 'text-lg font-semibold leading-6 text-font_primary underline decoration-bg-primary p-3'
+            ? 'text-lg font-semibold leading-6 text-font_primary underline decoration-text-primary p-3'
             : 'text-lg font-semibold leading-6 text-font_primary p-3';
 @endphp
 
