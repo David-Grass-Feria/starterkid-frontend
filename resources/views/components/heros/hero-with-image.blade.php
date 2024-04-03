042fe8d41e355dea00e4b352b64af3a3f4efc61f
David <kontakt@david-grass-feria.de>
2024-04-03 05:50:47 +0200

    Update hero-with-image.blade.php

--- resources/views/components/heros/hero-with-image.blade.php
+++ resources/views/components/heros/hero-with-image.blade.php
@@ -3,7 +3,7 @@
     <div class="relative isolate">
      
       <div class="mx-auto lg:flex lg:items-center lg:gap-x-10">
-        <div class="mx-auto">
+        <div class="mx-auto h-screen xl:h-auto">
         
           <h1 class="mt-10 text-center text-4xl font-bold text-font_primary xl:text-8xl">{{$h1}} <br> <span class="text-primary">{{$h1Color}}</span></h1>
           <p class="mt-6 w.full md:max-w-2xl mx-auto text-lg leading-8 text-font_primary text-center">{{$description}}</p>
@@ -14,7 +14,8 @@
            
         </div>
         </div>
-        <div class="mt-16 sm:mt-24 lg:mt-0 lg:flex-shrink-0 lg:flex-grow">
+        <div class="lg:flex-shrink-0 lg:flex-grow">
+          <img width="{{config('starterkid.image_width_height_attributes.medium.width')}}" height="{{config('starterkid.image_width_height_attributes.medium.height')}}" src="{{$imgSrc}}" alt="{{$imgAlt}}" class="rounded-3xl shadow-lg ring-1 ring-primary w-full md:max-w-md mx-auto xl:max-w-lg">
         </div>
       </div>
     </div>
