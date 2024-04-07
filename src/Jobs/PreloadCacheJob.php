b4966b27903182db58c24caa08b17ca40eca3baa
David <kontakt@david-grass-feria.de>
2024-04-07 07:00:57 +0200

    Create PreloadCacheJob.php

---
+++ src/Jobs/PreloadCacheJob.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace GrassFeria\StarterkidFrontend\Jobs;
+
+use ZipArchive;
+use Illuminate\Bus\Queueable;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+
+class PreloadCacheJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+   
+    public $url;
+    
+    public function __construct($url)
+    {
+        $this->url = $url;
+    }
+
+    public function handle()
+    {
+        // Versuche, die URL zu erreichen, um den Cache neu zu erstellen
+        Http::timeout(30)->get($this->url);
+    }
+}
