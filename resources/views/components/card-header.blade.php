16d3734d0b6945dc93656ff23cebd186e549f325
David <kontakt@david-grass-feria.de>
2024-04-09 22:14:58 +0200

    Update card-header.blade.php

--- resources/views/components/card-header.blade.php
+++ resources/views/components/card-header.blade.php
@@ -3,7 +3,7 @@
       
     
       <div>
-        <h1 class="text-4xl font-bold tracking-tight text-font_primary sm:text-6xl xl:text-6xl">{{$heading}}</h1>
+        <h1 class="text-4xl font-bold text-font_primary sm:text-6xl xl:text-6xl">{{$heading}}</h1>
         @if(isset($description))
         <p class="mt-6 text-lg leading-8 text-font_primary">{{$description}}</p>
         @endif
