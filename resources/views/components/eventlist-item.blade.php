c68c0e37ef8b79c379865305f3cb8b6d89649dd5
David <kontakt@david-grass-feria.de>
2024-04-09 07:30:06 +0200

    Update eventlist-item.blade.php

--- resources/views/components/eventlist-item.blade.php
+++ resources/views/components/eventlist-item.blade.php
@@ -9,16 +9,16 @@
     </div>
       
 
-    <div class="col-span-6 xl:col-span-8">
+    <div class="col-span-7 xl:col-span-8">
        <div class="flex flex-col">
-        <h2 class="text-xl font-bold text-font_primary">{{$eventName}}</h2>
+        <h2 class="text-md xl:text-lg font-bold text-font_primary">{{$eventName}}</h2>
         <p class="text-xs text-font_primary">{!!$eventTime!!}</p>
         <p class="text-xs text-font_primary">{{$eventAdress}}</p>
         <p class="text-xs text-font_primary">{{$eventCity}}</p>
        </div>
     </div>
 
-    <div class="col-span-4 xl:col-span-2">
+    <div class="col-span-3 xl:col-span-2">
         @if($firstLoop)
        <img rel="preload" as="image" width="80" height="80" class="w-20 rounded-md" src="{{$imgSrc}}" alt="{{$imgAlt}}" />
        @else
