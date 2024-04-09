5159d8b7a4e4cb709d0edf544a3d2d660122c535
David <kontakt@david-grass-feria.de>
2024-04-09 21:18:11 +0200

    1.0

---
+++ resources/views/components/iphone-homescreen.blade.php
@@ -0,0 +1,14 @@
+<div class="bg-white shadow-md ring-1 ring-primary rounded-3xl">
+    <div class="px-6 py-24 sm:px-6 sm:py-32 lg:px-8">
+      <div class="mx-auto max-w-2xl text-center">
+        <h2 class="text-xl font-bold text-font_primary xl:text-3xl">{{$heading}}</h2>
+        </div>
+        <div class="flex flex-col space-y-5 justify-center mt-10">
+          
+          <img class="w-64 rounded-md mx-auto" loading="lazy" src="{{asset('iphone1.webp')}}" />
+          <img class="w-64 rounded-md mx-auto" loading="lazy" src="{{asset('iphone2.webp')}}" />
+  
+         
+        </div>
+    </div>
+  </div>
