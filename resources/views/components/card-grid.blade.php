dbda092b2b9e1913a3f3cf67f4a0436cb019c208
David <kontakt@david-grass-feria.de>
2024-04-11 02:36:44 +0200

    Update card-grid.blade.php

--- resources/views/components/card-grid.blade.php
+++ resources/views/components/card-grid.blade.php
@@ -1,7 +1,7 @@
 <div>
     <div class="mx-auto">
     
-      <div class="mx-auto mt-5 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-5 xl:gap-y-20 lg:mx-0 lg:max-w-none sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3">
+      <div class="mx-auto mt-5 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-5 xl:gap-y-20 lg:mx-0 lg:max-w-none sm:grid-cols-2 lg:grid-cols-4 xl:grid-cols-4">
 {{$slot}}
       </div>
     </div>
