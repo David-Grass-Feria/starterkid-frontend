b3672fde9633f49e22211be34493f0b103a3cebb
David <kontakt@david-grass-feria.de>
2024-04-11 03:33:24 +0200

    1.0

--- resources/views/components/card-grid-service-item-target-blank.blade.php
+++ resources/views/components/card-grid-service-item-target-blank.blade.php
@@ -1,21 +1,24 @@
-<div class="relative flex flex-col space-x-3 rounded-lg border border-primary bg-white px-6 py-5">
-
- 
-  <div class="order-2">
-    @if($firstLoop)
-    <img rel="preload" as="image" width="40" height="40" class="h-10 w-10 rounded-3xl object-contain" src="{{$imgSrc}}" alt="{{$imgAlt}}">
-    @else
-    <img loading="lazy" width="40" height="40" class="h-10 w-10 rounded-3xl object-contain" src="{{$imgSrc}}" alt="{{$imgAlt}}">
-    @endif
+<div>
+  <div class="rounded-3xl group relative bg-white p-6 text-font_primary h-96 shadow-md">
+    <div>
+      <span class="inline-flex rounded-lg p-3">
+        @if($firstLoop)
+      <img rel="preload" as="image" width="40" height="40" class="h-10 w-10 rounded-3xl object-contain" src="{{$imgSrc}}" alt="{{$imgAlt}}">
+      @else
+      <img loading="lazy" width="40" height="40" class="h-10 w-10 rounded-3xl object-contain" src="{{$imgSrc}}" alt="{{$imgAlt}}">
+      @endif
+      </span>
+    </div>
+    <div class="mt-8">
+      <h3 class="text-base font-semibold leading-6">
+        <a target="_blank" href="{{$href}}" title="{{$hrefTitle}}" class="focus:outline-none">
+          <!-- Extend touch target to entire panel -->
+          <span class="absolute inset-0" aria-hidden="true"></span>
+          {{$heading}}
+        </a>
+      </h3>
+      <p class="mt-2 text-sm text-gray-500">{!! $preview !!}</p>
+    </div>
+   
   </div>
-  <div class="min-w-0 order-1 text-font_primary">
-    <a target="_blank" href="{{$href}}" title="{{$hrefTitle}}" class="focus:outline-none">
-      <span class="absolute inset-0" aria-hidden="true"></span>
-      <p class="text-lg font-bold">{{$heading}}</p>
-      <p class="truncate text-xs">{!! $preview !!}</p>
-    </a>
-  </div>
-
-
 </div>
-
