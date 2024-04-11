dfa6652e81115946846455cc45caf0a758affb2d
David <kontakt@david-grass-feria.de>
2024-04-11 03:19:04 +0200

    Update card-grid-service-item-target-blank.blade.php

--- resources/views/components/card-grid-service-item-target-blank.blade.php
+++ resources/views/components/card-grid-service-item-target-blank.blade.php
@@ -1,20 +1,21 @@
-<div class="relative flex items-center space-x-3 rounded-lg border border-primary bg-white px-6 py-5">
+<div class="relative flex flex-col space-x-3 rounded-lg border border-primary bg-white px-6 py-5">
 
  
-    <div class="flex-shrink-0">
-      @if($firstLoop)
-      <img rel="preload" as="image" width="40" height="40" class="h-10 w-10 rounded-3xl object-contain" src="{{$imgSrc}}" alt="{{$imgAlt}}">
-      @else
-      <img loading="lazy" width="40" height="40" class="h-10 w-10 rounded-3xl object-contain" src="{{$imgSrc}}" alt="{{$imgAlt}}">
-      @endif
-    </div>
-    <div class="min-w-0 flex-1 text-font_primary">
-      <a target="_blank" href="{{$href}}" title="{{$hrefTitle}}" class="focus:outline-none">
-        <span class="absolute inset-0" aria-hidden="true"></span>
-        <p class="text-lg font-bold">{{$heading}}</p>
-        <p class="truncate text-xs">{!! $preview !!}</p>
-      </a>
-    </div>
- 
+  <div class="order-2">
+    @if($firstLoop)
+    <img rel="preload" as="image" width="40" height="40" class="h-10 w-10 rounded-3xl object-contain" src="{{$imgSrc}}" alt="{{$imgAlt}}">
+    @else
+    <img loading="lazy" width="40" height="40" class="h-10 w-10 rounded-3xl object-contain" src="{{$imgSrc}}" alt="{{$imgAlt}}">
+    @endif
+  </div>
+  <div class="min-w-0 order-1 text-font_primary">
+    <a target="_blank" href="{{$href}}" title="{{$hrefTitle}}" class="focus:outline-none">
+      <span class="absolute inset-0" aria-hidden="true"></span>
+      <p class="text-lg font-bold">{{$heading}}</p>
+      <p class="truncate text-xs">{!! $preview !!}</p>
+    </a>
+  </div>
+
 
 </div>
+
