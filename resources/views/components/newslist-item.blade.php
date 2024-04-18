08a73a688e791963c573e803f24404731b68d0b4
David <kontakt@david-grass-feria.de>
2024-04-18 09:35:19 +0200

    Update newslist-item.blade.php

--- resources/views/components/newslist-item.blade.php
+++ resources/views/components/newslist-item.blade.php
@@ -22,16 +22,16 @@
         <span class="text-xs">{{$vendorName}}</span>
          </div> 
         <h2 class="text-md xl:text-lg font-bold text-font_primary">{{$title}}</h2>
-        <p class="text-xs text-font_primary">{!!$description!!}</p>
+        <p class="text-xs text-font_primary">{!!Str::limit($description,200)!!}</p>
        </a>
        </div>
     </div>
 
     <div class="col-span-3 xl:col-span-2">
         @if($firstLoop)
-       <img rel="preload" as="image" width="80" height="80" class="w-20 xl:w-44 rounded-md" src="{{$imgSrc}}" alt="{{$imgAlt}}" />
+       <img rel="preload" as="image" width="80" height="80" class="max-w-20 xl:w-44 rounded-md" src="{{$imgSrc}}" alt="{{$imgAlt}}" />
        @else
-       <img loading="lazy" width="80" height="80" class="w-20 xl:w-44 rounded-md" src="{{$imgSrc}}" alt="{{$imgAlt}}" />
+       <img loading="lazy" width="80" height="80" class="max-w-20 xl:w-44 rounded-md" src="{{$imgSrc}}" alt="{{$imgAlt}}" />
        @endif
     </div>
     
