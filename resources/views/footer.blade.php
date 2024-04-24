d1783685af6cc86e4759439ec377e09391544752
David <kontakt@david-grass-feria.de>
2024-04-24 20:50:20 +0200

    Update footer.blade.php

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -82,7 +82,7 @@
         <div class="mt-16 grid grid-cols-2 gap-8 xl:col-span-2 xl:mt-0">
           <div class="md:grid md:grid-cols-2 md:gap-8">
             <div>
-              <h3 class="text-sm font-semibold leading-6 text-gray-900">{{config('starterkid-service.service_title')}}</h3>
+              <h3 class="text-sm font-semibold leading-6 text-gray-900">{{config('starterkid-service.service_menu_name')}}</h3>
               <ul role="list" class="mt-6 space-y-4">
                 @foreach($services as $service)
                 <x-starterkid-frontend::footer-link wire:navigate href="{{route('front.service.show',$service->slug)}}" title="{{$service->title}}">{{$service->name}}</x-starterkid-frontend::footer-link>
