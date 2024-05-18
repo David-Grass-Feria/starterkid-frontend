99818ca85d2ccf844269866d4980ad6976f068e7
David <kontakt@david-grass-feria.de>
2024-05-18 20:00:12 +0200

    Update PreloadCacheJob.php

--- src/Jobs/PreloadCacheJob.php
+++ src/Jobs/PreloadCacheJob.php
@@ -27,6 +27,7 @@ class PreloadCacheJob implements ShouldQueue
     {
         // Versuche, die URL zu erreichen, um den Cache neu zu erstellen
         Http::get($this->url);
+        Http::get($this->url);
         
     }
 }
